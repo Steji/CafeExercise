3a2a50b5b4299dce6c60e03218203682eb464284
Ste Jervis <ste.jervis@gmail.com>
2019-11-25 20:48:30 -0500

    Added console

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using Cafe;
+﻿using System.Linq;
+using Cafe;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Console
@@ -12,9 +13,27 @@ namespace Console
 
             var menu = serviceProvider.GetService<IMenu>();
 
-            //menu.add
-            //menu.select
-            //menu.total
+            menu.Add(new Item { Name = "Cola", Temperature = Temperature.Cold, Price = 0.5m });
+            menu.Add(new Item { Name = "Coffee", Temperature = Temperature.Hot, Price = 1.0m });
+            menu.Add(new Item { Name = "Cheese Sandwich", Temperature = Temperature.Cold, Price = 2.0m });
+            menu.Add(new Item { Name = "Steak Sandwich", Temperature = Temperature.Hot, Price = 4.50m });
+
+            System.Console.WriteLine("Please enter order separated with commas (e.g. Cola, Cheese Sandwich) ");
+            var input = System.Console.ReadLine();
+
+            foreach (var item in input.Split(","))
+                menu.Select(item.Trim());
+
+            var total = menu.CalculateTotal().ToString("0.00");
+
+            System.Console.WriteLine("Your total is: £{0}", total);
+
+            System.Console.WriteLine("Thank you! (Press enter to finish...)");
+            System.Console.ReadLine();
+
+            //Not in spec but should have some validation when item is being ordered off menu
+            //Selection should probably be case insensitive
+            //Should have a nice UI not a console
         }
 
         private static IServiceCollection ConfigureServices()
